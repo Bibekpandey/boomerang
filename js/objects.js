a37b3cbb1fef213495f2f9db4f1982405a5b81c2
Bibekpandey <spirit_bibek@yahoo.com>
2016-05-26 22:43:25 +0545

    basic object movement done

--- js/objects.js
+++ js/objects.js
@@ -14,14 +14,19 @@ function GameObject(options) {
     obj.type = options.type || "GameObject";
     obj.name = options.name || "NoName";
     obj.frameIndex=0;
-        obj.tickCount=0;
-        obj.ticksPerFrame=2;
-        obj.numberOfFrames = options.numberOfFrames || 1;
-        obj.dx=0,obj.dy=0;
+    obj.tickCount=0;
+    obj.ticksPerFrame=options.ticksPerFrame || 2;
+    obj.frameDetails = options.frameDetails;
+    obj.dx=0,
+    obj.dy=0;
+
     obj.velocity = function (t) {
         var vel = {vx:0,vy:0};
         return vel;
     };
+
+    obj.x= obj.y=0;
+
     obj.accn_func = function(t) {};
 
     obj.context = options.context;
@@ -29,26 +34,38 @@ function GameObject(options) {
     obj.height= options.height;
     obj.image = options.image;
 
+    obj.frameWidth = options.frameWidth || this.width;
+    obj.frameHeight= options.frameHeight || this.height;
+
+    obj.currentAction = options.currentAction;
+    obj.currentFrame = 0;
+
     obj.render = function() {
         obj.context.drawImage(obj.image, 
-            this.frameIndex * this.width / this.numberOfFrames,
-            0, 
-            this.width / this.numberOfFrames, 
-            this.height,
+
+            this.currentFrame * this.width/13,
+
+            this.frameDetails.action[this.currentAction] * 
+                    this.height / this.frameDetails.numActions, 
+
+            this.width / 13,//this.frameDetails.numFrames[this.currentAction], 
+            this.height/21,
             this.x,
             this.y,
-            this.width / this.numberOfFrames,
-            this.height
+            this.width / 13,
+            this.height/21
         );
     };
 
     obj.update = function() {
-        this.tickCount+=1;
-        if(this.tickCount>this.ticksPerFrame) {
-            this.tickCount=0;
-            this.frameIndex = (this.frameIndex+1)%this.numberOfFrames;
-        }
-    };
+            //this.tickCount+=1;
+            if(this.tickCount > this.ticksPerFrame) {
+                this.tickCount=0;
+                this.currentFrame = (this.currentFrame+1) %
+                    this.frameDetails.numFrames[this.currentAction];
+            }
+        };
+
 
     obj.getRect = function () {
         return {x1:this.x,
@@ -65,31 +82,31 @@ function checkCollision(objects) {
         for(var y=x+1;y<objects.length;y++) {
             if(collisionRect(objects[x].getRect(), 
                                         objects[y].getRect())) {
-                console.log('Collision :' + objects[x].name+ ' and ' + objects[y].name);
-                console.log(JSON.stringify(objects[x].getRect()) + ' ' + JSON.stringify(objects[y].getRect()));
+                var stat='Collision :' + objects[x].name+ ' and ' + objects[y].name;
+                stat+=('<br>'+JSON.stringify(objects[x].getRect()) + ' ' + JSON.stringify(objects[y].getRect()));
+                $('span').text(stat);
             }
+            else $('span').text('');
         }
     }
 }
 
 function collisionRect(rectA, rectB) {
-    // check diagonals of any rectangle crosses sides of other
-    var d1 = line(rectA.x1,rectA.y1,rectA.x2,rectA.y2),
-        d2 = line(rectA.x2,rectA.y1, rectA.x1,rectA.y2);
-
-    // rectB's xs and ys
-    var x1=rectB.x1, x2=rectB.x2, y1=rectB.y1, y2=rectB.y2;
-
-    return linesIntersect(d1, line(x1,y1,x2,y1)) ||
-            linesIntersect(d1, line(x2,y1,x2,y2)) ||
-            linesIntersect(d1, line(x2,y2,x1,y2)) ||
-            linesIntersect(d1, line(x1,y2,x1,y1)) ||
-            linesIntersect(d2, line(x1,y1,x2,y1)) ||
-            linesIntersect(d2, line(x2,y1,x2,y2)) ||
-            linesIntersect(d2, line(x2,y2,x1,y2)) ||
-            linesIntersect(d2, line(x1,y2,x1,y1));
+    // Just check if any corner of rectA lies within rect B
+    // might not work for some special conditions, which are
+    // not expected to occur in the game
+    return pointInside(rectA.x1,rectA.y1, rectB) ||
+        pointInside(rectA.x2, rectA.y1, rectB) ||
+        pointInside(rectA.x2, rectA.y2, rectB) ||
+        pointInside(rectA.x1, rectA.y2, rectB);
 }
 
+// check if point lies inside rect or not
+function pointInside(x, y, rect) {
+    return rect.x1<=x && rect.x2>=x && rect.y1<=y && rect.y2>=y;
+}
+
+// checking if lines intersect or not.. not working well
 function linesIntersect(lineA, lineB) {
     // idea is to put two points of a line into the eqn or other
     // and if product is <=0 intersect else false
